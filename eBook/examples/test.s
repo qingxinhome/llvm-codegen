66ed04a8e40193d21824693533377b5b2f70fb2f
qingxinhome <758355272@qq.com>
2025-09-21 22:33:07 +0800

    add test code and document

---
+++ eBook/examples/test.s
@@ -0,0 +1,16 @@
+	.text
+	.file	"test.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:                                # %entry
+	xorl	%eax, %eax
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.ident	"clang version 18.1.8 (https://github.com/llvm/llvm-project.git 3b5b5c1ec4a3095ab096dd780e84d7ab81f3d7ff)"
+	.section	".note.GNU-stack","",@progbits
